076b364efdb236e198f5b327192c0a5782b2aebf
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-08-25 13:02:41 -0700

    Add files via upload
    Done with Master the While Loop in JavaScript!

---
+++ index.js
@@ -0,0 +1,39 @@
+// chapter 41: while loop
+
+// Difference between a for loop and while loop 
+
+// for loop: a for loop is an iteration method that is best used when you know the number of iteration ahead
+// of time . it's always followed by the initiialization, expression and increment or decrement statments. 
+
+// while loop: A while loop is an iteration method that is best used when you don't know the number of 
+// iteration ahead of time. the contents of the loop are executed as long as the expression evaluates to true.
+
+// Example for loop
+
+// for ( count = 1; count < 10; count++) {
+//     console.log(count);
+// }
+
+// Note: The increament does not have to be ++ , but you'll see that the most often.
+
+// same example with while loop
+
+// let count = 1;
+// while (count < 10) {
+//     console.log(count);
+//     count++;
+// }
+
+// For vs while syntax 
+
+// so, in summary , the while loop has a looser syntax , and the for loop has a more rigid syntax . A while loop
+// expects some sort of modification to the variable in the body , whereas everything is in the for loop's
+// // definitin
+
+// A few use cases 
+
+// use a for loop to iterate over an array 
+// use a for loop when you know the loop should execute n times. 
+// use a while loop for reading a file into a variable .
+// // use a while loop when asking for user input. 
+// use a while loop when the increment value is nonstandard.
